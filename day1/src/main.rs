fc56b18520783488ed48b4ac1077c79dffc27c2e
Rasmus Steen <rasmus@eventful.se>
2021-12-02 10:02:35 +0100

    fixing day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -8,64 +8,57 @@ fn main() {
 }
 
 fn part1() {
-    if let Ok(lines) = read_lines("./input.txt") {
-        let mut counter = -1;
-        let mut last_number = -1;
-        for line in lines {
-            if let Ok(value) = line {
-                let number = value.parse::<i32>().unwrap();
-
-                if last_number < number {
-                    counter += 1;
-                }
-
-                last_number = number;
-            }
+    let input = read_input("./input.txt");
+    
+    let mut counter: i32 = 0;
+    for i in 1..input.len() {
+        if input[i - 1] < input[i] {
+            counter += 1;
         }
-
-        println!("Answer part 1: {}", counter)
     }
+
+    println!("Answer part 1: {}", counter);
 }
 
 fn part2() {
-    if let Ok(lines) = read_lines("./input.txt") {
-        let mut counter: i32 = 0;
-        let mut sums: Vec<i32> = Vec::new();
-        let mut index: usize = 0;
-        for line in lines {
-            if let Ok(value) = line {
-                let number = value.parse::<i32>().unwrap();
-                sums.push(number);
+    let input = read_input("./input.txt");
+    let input_len = input.len();
+    let mut sums: Vec<i32> = vec![0; input_len];
+    let mut counter: i32 = 0;
 
-                if index > 0 {
-                    sums[index - 1] += number;
-                }
-
-                if index > 1 {
-                    sums[index - 2] += number;
-                }
-
-                if index > 3 && sums[index - 2] > sums[index - 3] {
-                    counter += 1;
-                }
-
-                index += 1;
-            }
+    for i in 0..input_len {
+        let inner_start = if i < 2 { 0 } else { i - 2 };
+        for j in inner_start..i + 1 {
+            sums[j] += input[i];
         }
-
-        if sums[index - 2] > sums[index - 3] {
-            counter += 1;
-        }
-
-        if sums[index - 1] > sums[index - 2] {
-            counter += 1;
-        }
-
-        println!("Answer part 2: {}", counter);
     }
+
+    for i in 1..sums.len() {
+        if sums[i - 1] < sums[i] {
+            counter += 1;
+        }
+    }
+
+    println!("Answer part 2: {}", counter);
 }
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>, {
+fn read_input<P>(filename: P) -> Vec<i32> where P: AsRef<Path> + std::fmt::Display + Copy {
+    let mut parsed_input: Vec<i32> = Vec::new();
+    if let Ok(lines) = read_lines(filename) {
+        for line_stream in lines {
+            if let Ok(line) = line_stream {
+                parsed_input.push(line.parse::<i32>().unwrap());
+            } else {
+                panic!("Could not process file {}", filename);
+            }
+        }
+    } else {
+        panic!("Failed to read file {}", filename);
+    }
+    return parsed_input;
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path> {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
