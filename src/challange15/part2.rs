fb6963e2c4a49416715e388804ddc65d75d64546
Rasmus Steen <rasmus@eventful.se>
2021-12-15 18:37:29 +0100

    day15 incomplete

--- src/challange15/part2.rs
+++ src/challange15/part2.rs
@@ -1,14 +1,49 @@
 use crate::challange15::input::ChallangeInput15;
+use crate::challange15::node::Node;
+use crate::challange15::util;
 
 pub fn run(input: ChallangeInput15) -> String {
-    "input.text".to_string()
+    let mut large = enlarge_input(&input, 5);
+    large.nodes[0].path_risk = 0;
+
+    if let Some(path_risk) = util::calculate_min_path_risk(&mut large.nodes, large.width) {
+        path_risk.to_string()
+    } else {
+        String::from("shit")
+    }
 }
 
 fn enlarge_input(input: &ChallangeInput15, multiplier: usize) -> ChallangeInput15 {
+    let source_rows = input.nodes.len() / input.width;
     let width = input.width * multiplier;
-    let mut new_input: Vec<Node> = vec![0; nodes.len() * multiplier * multiplier];
+    let length = input.nodes.len() * multiplier * multiplier;
+    let mut nodes: Vec<Node> = Vec::new();
 
-    for i in 0..new_input.len() {
-        let mult_1 = i / input.width;
+    for i in 0..length {
+        let row = i / width;
+        let block_row = row / source_rows;
+        let source_index = i % input.width + (row % source_rows) * input.width;
+        let add: u32 = (i % width / input.width + block_row) as u32;
+        let new_node = Node {
+            risk_level: calculate_risk_level(&input.nodes[source_index], add),
+            previous: 0,
+            path_risk: u32::MAX,
+        };
+
+        nodes.push(new_node);
+    }
+
+    let mut new_input = ChallangeInput15::default();
+    new_input.width = width;
+    new_input.nodes = nodes;
+    new_input
+}
+
+fn calculate_risk_level(node: &Node, add: u32) -> u32 {
+    let risk_level = (node.risk_level + add) % 10 + 1;
+    if risk_level == 10 {
+        1
+    } else {
+        risk_level
     }
 }
--- src/challange15/util.rs
+++ src/challange15/util.rs
@@ -1,20 +1,26 @@
 use crate::challange15::node::Node;
+use colored::*;
+use std::collections::HashSet;
 
 pub fn calculate_min_path_risk(nodes: &mut Vec<Node>, width: usize) -> Option<u32> {
-    let mut next: Vec<usize> = vec![0];
+    let mut nexts: Vec<HashSet<usize>> = vec![HashSet::from([1, width])];
 
-    while let Some(index) = next.pop() {
-        let neighbours = get_neighbours(nodes.len(), width, index);
+    while let Some(indicies) = nexts.pop() {
+        let mut next: HashSet<usize> = HashSet::new();
 
-        for i in neighbours {
-            let new_risk = nodes[index].path_risk + nodes[i].risk_level;
-            if new_risk < nodes[i].path_risk {
-                nodes[i].path_risk = new_risk;
-                nodes[i].previous = index;
-                next.push(i)
-            }
+        for index in indicies {
+            let previous_index = get_min_risk_preceding_neighbour(&nodes, width, index);
+            nodes[index].previous = previous_index;
+            nodes[index].path_risk = nodes[index].risk_level + nodes[previous_index].path_risk;
+            insert_next_neighbours(&mut next, nodes.len(), width, index);
+        }
+
+        if next.len() > 0 {
+            nexts.push(next);
         }
     }
+
+    print_path(nodes, width);
 
     if let Some(last) = nodes.iter().last() {
         Some(last.path_risk)
@@ -23,24 +29,60 @@ pub fn calculate_min_path_risk(nodes: &mut Vec<Node>, width: usize) -> Option<u3
     }
 }
 
-pub fn get_neighbours(length: usize, width: usize, index: usize) -> Vec<usize> {
-    let mut neighbours: Vec<usize> = Vec::new();
+pub fn get_min_risk_preceding_neighbour(nodes: &Vec<Node>, width: usize, index: usize) -> usize {
+    let up_allowed = index >= width;
+    let left_allowed = index % width > 0;
 
-    if index >= width {
-        neighbours.push(index - width);
+    if up_allowed && left_allowed {
+        if nodes[index - width].path_risk < nodes[index - 1].path_risk {
+            index - width
+        } else {
+            index - 1
+        }
+    } else if up_allowed {
+        index - width
+    } else if left_allowed {
+        index - 1
+    } else {
+        0
     }
+}
 
+pub fn insert_next_neighbours(
+    next: &mut HashSet<usize>,
+    length: usize,
+    width: usize,
+    index: usize,
+) {
     if index + width < length {
-        neighbours.push(index + width);
-    }
-
-    if index % width > 0 {
-        neighbours.push(index - 1);
+        next.insert(index + width);
     }
 
     if index % width < width - 1 {
-        neighbours.push(index + 1);
+        next.insert(index + 1);
+    }
+}
+
+fn print_path(nodes: &Vec<Node>, width: usize) {
+    let mut path: HashSet<usize> = HashSet::from([nodes.len() - 1, 0]);
+    let mut previous = nodes.iter().last().unwrap();
+
+    while previous.previous > 0 {
+        path.insert(previous.previous);
+        previous = &nodes[previous.previous];
     }
 
-    neighbours
+    for i in 0..nodes.len() {
+        if i % width == 0 {
+            println!();
+        }
+
+        if path.contains(&i) {
+            print!("{}", nodes[i].risk_level.to_string().cyan());
+        } else {
+            print!("{}", nodes[i].risk_level.to_string());
+        }
+    }
+
+    println!();
 }
